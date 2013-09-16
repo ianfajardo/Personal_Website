bac7679d9b6b8c3ad449a3f6d987833bde3f960f
Ian Fajardo <ianfajardozx@gmail.com>
2013-09-16 01:00:37 -0400

    Layout Tweaks
    Made navbar responsive, layout blocks, adding information for about
    section

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 	</head>
 	<body>
 		<div class="container">
-			<nav class="top-nav pull-right">
+			<nav class="top-nav">
 				<ul>
 					<li><a href="">Portfolio</a></li>
 					<li><a href="">About</a></li>
@@ -50,24 +50,42 @@
 				</div>
 			</div>
 		</div>
+		<div class="info">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-sm-4">
+						<div style="text-align: center">
+							<h2>About</h2>
+							<img src="assets/img/mypic.jpg">
+							<h3>ARIEL-IAN &nbsp FAJARDO</h3>
+							<h4>DEVELOPER + DESIGNER</h4>
+							<h5>University of Virginia // B.S. in Computer Science //2013 </h5>
+						</div>
+						<p>I am a computer science major and interested in web and mobile development. My interests lie in front-end development such as creating websites, user interfaces, and many more.  In addition, I dabble in developing mobile applications for iOS and Android.
+						</p>
+					</div>
+					<div class="col-md-6 col-sm-8">
+						<div style="text-align: center">
+						<h2>Blog</h2>
+						
+						</div>
+						
+					</div>
+				
+				</div>
+			</div>
+		</div>
 
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 col-sm-4">
-					<h1>About</h1>
-				</div>
-				<div class="col-md-6 col-sm-8">
-					<h1>Blog</h1>
-				</div>
-			
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<h1>Photos</h1>
-					<div id="instafeed"></div>
+		<div class="photos">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>Photos</h2>
+						<div id="instafeed">
+						</div>
+					</div>
 				</div>
 			</div>
-		
 		</div>
 
 		<script src="assets/js/jquery-1.10.2.min.js"></script>
